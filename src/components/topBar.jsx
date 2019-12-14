718184f47bb4c656686156cb3ca54cdc1b0d5d77
Alejandro <aled98.ad@gmail.com>
2019-12-14 14:17:56 -0600

    added topbar

---
+++ src/components/topBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { NavBar } from 'react-bootstrap/Navbar';
+import ReactDOM from 'react-dom';
+
+class topBar extends Component {
+    render() {
+        return <div>
+
+        </div>;
+    }
+}
+
+export default topBar;
