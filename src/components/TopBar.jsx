f668aeff9148a433cff1e77b70bd334f59f04c40
Alejandro <aled98.ad@gmail.com>
2019-12-14 14:51:42 -0600

    setting main component

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import NavBar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+class TopBar extends Component {
+    render() {
+        return <div>
+            <NavBar bg="dark" variant="dark" expand="lg">
+                <NavBar.Brand href="#">
+                    <b> Expense Tracker </b>
+                </NavBar.Brand>
+                <NavBar.Collapse>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#">
+                            Categories
+                        </Nav.Link>
+                        <Nav.Link href="#">
+                            Payments
+                        </Nav.Link>
+                    </Nav>
+                </NavBar.Collapse>
+            </NavBar>
+        </div>;
+    }
+}
+
+export default TopBar;
