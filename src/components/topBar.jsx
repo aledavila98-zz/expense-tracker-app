04e141ee8d72e5606341c7d3561aae29e2fd2871
Alejandro <aled98.ad@gmail.com>
2019-12-14 14:36:06 -0600

    trying to render topbar

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { NavBar } from 'react-bootstrap/Navbar';
-import ReactDOM from 'react-dom';
+import NavBar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 class topBar extends Component {
     render() {
-        return <div>
-
-        </div>;
+        return <NavBar bg="dark" expand="lg">
+            <NavBar.Brand href="#">
+                <b> Expense Tracker </b>
+            </NavBar.Brand>
+            <NavBar.Collapse>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#">
+                        Categories
+                    </Nav.Link>
+                    <Nav.Link href="#">
+                        Payments
+                    </Nav.Link>
+                </Nav>
+            </NavBar.Collapse>
+        </NavBar>;
     }
 }
 
