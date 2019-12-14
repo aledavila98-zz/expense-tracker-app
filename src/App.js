e49f1c1c467313ce0e65d575aad235f4c6b6611b
Alejandro <aled98.ad@gmail.com>
2019-12-14 15:06:52 -0600

    added method for mapping categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopBar from './components/TopBar';
+import Main from './components/Main';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Main />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,1 +1,33 @@
 import React, { Component } from "react";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+const renderCategories = () => {
+    const categories = [
+        'Bike',
+        'Car',
+        'Plane'
+    ];
+    return categories.map(category => {
+        return <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="0"> {category} </Accordion.Toggle>
+        </Card.Header>;
+    })
+} 
+
+class Main extends Component {
+    render () {
+        return <div>
+            <Accordion defaultActiveKey="0">
+                <Card>
+                    <Card.Body>
+                        { renderCategories() }
+                    </Card.Body>
+                </Card>
+            </Accordion>
+        </div>;
+    }
+}
+
+export default Main;
