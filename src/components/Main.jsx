e4cf3b0e638a9af1cc93db0b23b7fcb41d2fac2d
Alejandro <aled98.ad@gmail.com>
2019-12-14 15:20:42 -0600

    adding payments

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,12 +9,35 @@ const renderCategories = () => {
         'Car',
         'Plane'
     ];
-    return categories.map(category => {
+    return categories.map( (category, index) => {
         return <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0"> {category} </Accordion.Toggle>
+            <Accordion.Toggle as={Button} variant="link" eventKey={index}> {category} </Accordion.Toggle>
+            {/* <Accordion.Collapse eventKey={index}>
+                {/* { renderPayments() } */}
+            </Accordion.Collapse> */}
         </Card.Header>;
     })
-} 
+}
+
+const renderPayments = () => {
+    const payments = [
+        {
+            paymentName: "Pago 1",
+            observations: "---",
+            amount: 200
+        },
+        {
+            paymentName: "Pago 2",
+            observations: "---",
+            amount: 100
+        }
+    ];
+    return payments.map(payment => {
+        return <Card.Body>
+            <b> {payment.paymentName} </b> - {payment.observations} - {payment.amount.toFixed(2)}
+        </Card.Body>;
+    });
+}
 
 class Main extends Component {
     render () {
