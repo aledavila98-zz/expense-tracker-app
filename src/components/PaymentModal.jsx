074501f6d01e35d4efe7214ebd35f088ab8326c1
Alejandro <aled98.ad@gmail.com>
2019-12-15 14:44:35 -0600

    added payment modal

--- src/components/PaymentModal.jsx
+++ src/components/PaymentModal.jsx
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class PaymentModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            isVisible: props.isVisible,
+            categoryId: props.categoryId,
+            crud: props.crud 
+        };
+    }
+
+    handleClose = () => {
+        this.setState({
+            isVisible: false
+        });
+    }
+
+    commitCategory = (op) => {
+        this.setState({
+            isVisible: false
+        });
+    }
+
     render() {
-        return <div></div>;
+        const title = this.state.crud === 0 ? 'Create Payment' : 'Update Payment';
+        const btnText = this.state.crud === 0 ? 'Create' : 'Update';
+        return <div>
+            <Modal show={this.state.isVisible} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title> {title}  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formCategory">
+                            <Form.Label> Amount </Form.Label>
+                            <Form.Control type="number" placeholder="Enter amount" />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" size="sm" onClick={(e) => this.commitCategory(this.state.crud)}> {btnText} </Button>
+                    <Button variant="danger" size="sm" onClick={this.handleClose}> Discard </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>;
     }
 }
 
