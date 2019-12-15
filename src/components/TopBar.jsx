b098a7ca02d8d03dcacd9e4a2d730e40ce3f9ac2
Alejandro <aled98.ad@gmail.com>
2019-12-15 11:01:42 -0600

    continue work with states

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,35 +3,45 @@ import NavBar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 class TopBar extends Component {
-    
-    handleClick = () => {
-        // let route = '';
-        // if (screen === 0) {
-        //     route = 'CategoriesPayments';
-        // } else if (screen === 1) {
-        //     route = 'Categories';
-        // } else if (screen === 2) {
-        //     route = 'Payments';
-        // }
 
-        // this.setState({
-        //     screen: route
-        // });
-        console.log(this.state)
+    constructor(props) {
+        super(props);
+        this.state = {
+            screen: 'CategoriesPayments'
+        };
+
+        this.handleClick = this.handleClick.bind(0);
+    }
+    
+    handleClick = (screen) => {
+        console.log(screen)
+        let route = '';
+        if (screen === 0) {
+            route = 'CategoriesPayments';
+        } else if (screen === 1) {
+            route = 'Categories';
+        } else if (screen === 2) {
+            route = 'Payments';
+        }
+
+        this.setState({
+            screen: route
+        });
+        console.log(this.state);
     }
 
     render() {
         return <div>
             <NavBar bg="dark" variant="dark" expand="lg">
-                <NavBar.Brand href="#" key={0} onClick={this.handleClick}>
+                <NavBar.Brand href="#" onClick={this.handleClick.bind(0)}>
                     <b> Expense Tracker </b>
                 </NavBar.Brand>
                 <NavBar.Collapse>
                     <Nav className="mr-auto">
-                        <Nav.Link href="#" key={1} onClick={this.handleClick}>
+                        <Nav.Link href="#" onClick={this.handleClick.bind(1)}>
                             Categories
                         </Nav.Link>
-                        <Nav.Link href="#" key={2} onClick={this.handleClick}>
+                        <Nav.Link href="#" onClick={this.handleClick.bind(2)}>
                             Payments
                         </Nav.Link>
                     </Nav>
