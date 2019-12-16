b44cdb5a1e208f7c1c339e5942fafc2bfba299c6
Alejandro <aled98.ad@gmail.com>
2019-12-16 10:16:32 -0600

    fetch content in payments

--- src/components/Payments.jsx
+++ src/components/Payments.jsx
@@ -2,60 +2,66 @@ import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons/faPencilAlt";
+import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
+import axios from "axios";
 
 class Payments extends Component {
 
-    fetchData = () => {
-        
+    constructor(props) {
+        super(props);
+        this.state = {
+            payments_list: []
+        };
     }
 
-    fillTable = (data) => {
-        return data.map(payment => {
+    fetchData = async () => {
+        return axios.get("http://localhost:5000/api/Payment", {
+            responseType: "json"
+        }).then(res => res.data)
+        .catch(err => console.log(err) );
+    }
+
+    async componentDidMount() {
+        this.setState({ payments_list: await this.fetchData() });
+    }
+
+    fillTable = () => {
+        return this.state.payments_list.map(payment => {
             return <tr>
                 <td> {payment.paymentNum} </td>
-                <td> {payment.observations} </td>
                 <td> {payment.amount} </td>
-                <td> {payment.category_name} </td>
+                <td> --- </td>
+                <td>
+                    <ButtonGroup>
+                        <Button variant="primary" size="sm"> <FontAwesomeIcon icon={faPencilAlt} /> </Button>
+                        <Button variant="danger" size="sm"> <FontAwesomeIcon icon={faTrash} /> </Button>
+                    </ButtonGroup>     
+                </td>
             </tr>
         });
     }
 
     render() {
-        const payments = [
-            {
-                paymentNum: "# 3",
-                observations: "---",
-                amount: 5,
-                category_name: "Bikes"
-            },
-            {
-                paymentNum: "# 4",
-                observations: "Trying here",
-                amount: 10,
-                category_name: "Car"
-            }
-        ];
-
         return <div>
             <Card>
                 <Card.Header>
-                    <div className="row">
-                        <div className="col-sm-8" style={{alignContent: "right"}}> <h3> Payments </h3> </div>
-                        <div className="col-sm-4" style={{alignItems: "right"}}> <Button variant="primary" size="sm">New payment</Button> </div>
-                    </div>
+                    <h3> Payments </h3>
                 </Card.Header>
                 <Card.Body>
                     <Table>
                         <thead>
                             <tr>
                                 <th> Payment Number </th>
-                                <th> Observations </th>
                                 <th> Amount </th>
-                                <th> Category </th>    
+                                <th> Category </th>
+                                <th> </th>    
                             </tr>      
                         </thead>
                         <tbody>
-                            { this.fillTable(payments) }
+                            { this.fillTable() }
                         </tbody>    
                     </Table>  
                 </Card.Body>
